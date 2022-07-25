ca851d29ddff1e96f02eafe88b0027be2ad2c7a1
ZakiOtsutsuki <zakijamaliarafi@gmail.com>
2022-07-25 23:27:33 +0700

    open google

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome('/bin/chromedriver')
-driver.get("https://www.python.org")
+driver.get("https://www.google.com/")
