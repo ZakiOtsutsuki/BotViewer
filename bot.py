2d1362cac5bc85b06ad240d55da82a1aec0988e0
ZakiOtsutsuki <zakijamaliarafi@gmail.com>
2022-07-26 10:19:07 +0700

    add login function

--- bot.py
+++ bot.py
@@ -1,4 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome('/bin/chromedriver')
-driver.get("https://www.google.com/")
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+import time
+
+browser = uc.Chrome()
+
+username = 'zakijamaliarafi'
+password = 'zaki1745'
+
+def login():
+    browser.get('https://google.com')
+    browser.find_element(By.CSS_SELECTOR, '.gb_1').click()
+    time.sleep(3)
+    browser.find_element(By.CSS_SELECTOR, '#identifierId').send_keys(username)
+    time.sleep(1)
+    browser.find_element(By.CSS_SELECTOR, '.VfPpkd-LgbsSe-OWXEXe-k8QpJ > span:nth-child(4)').click()
+    time.sleep(3)
+    browser.find_element(By.CSS_SELECTOR, '#password > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)').send_keys(password)
+    time.sleep(1)
+    browser.find_element(By.CSS_SELECTOR, '.VfPpkd-LgbsSe-OWXEXe-k8QpJ > span:nth-child(4)').click()
+    time.sleep(10)
+
+login()
